de6f0875671d3ffc3033aeaac1953ac22dbe72e1
joshua <josh.d.armour@gmail.com>
2023-07-24 22:18:04 +1000

    deploy

--- blog/src/routes/Blog/+page.svelte
+++ blog/src/routes/Blog/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+
+</script>
 <p class="text">
     Making this blog site because I wanted to see if SvelteKit was any good,
     so far so good.
@@ -5,13 +9,13 @@
 <div class="images">
     <ol style="border: 1px; color:white;">
         <li>   
-            <img src="/blogDev1.png" width="100%" height="auto">
+            <img src="{base}/blogDev1.png" width="100%" height="auto">
         </li>
         <li>
             I like the minimalist look of black and white.
         </li>
         <li>
-            <img src="/blogDev2.png" width="100%" height="auto">
+            <img src="{base}/blogDev2.png" width="100%" height="auto">
         </li>
     </ol>
 </div>
