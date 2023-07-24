455c9aac7b1e77e5191117c5cf46dd374fb9a099
joshua <josh.d.armour@gmail.com>
2023-07-25 02:15:09 +1000

    Updates

---
+++ _app/immutable/nodes/2.c672d23b.js
@@ -0,0 +1,18 @@
+import{s as o,p as a}from"../chunks/scheduler.63fd0e0f.js";import{S as l,i as r,g as h,h as c,G as m,a as g,f as d}from"../chunks/index.69766d95.js";function p(n){let e,i=`<h1 class="headingStyle">Welcome to my Blog</h1> <div class="messageContainer"><h3 style="color: aliceblue; font-size: 26px;">ChatGPT says</h3> <p class="message" style="color: aliceblue;">Welcome to my website! This is the hub of my creative endeavors, where I showcase a variety of exciting projects
+            that I have in the works. As a passionate individual with a love for all things tech and gaming, you&#39;ll find a collection of captivating content here. 
+            Dive into the world of immersive gaming with my Unity game, explore the latest updates and insights 
+            on my blog, and engage with like-minded enthusiasts in the interactive chatroom. Whether you&#39;re a fellow 
+            developer, an avid gamer, or simply someone seeking inspiration, this blog is the perfect place to connect, 
+            learn, and share experiences. Join me on this thrilling journey as we unravel the mysteries of game development, 
+            engage in thoughtful discussions, and embark on exciting new ventures together. Let&#39;s make magic happen, one project at a time!</p> <h3 style="color: aliceblue; font-size: 26px;">Thank you ChatGPT very cool</h3></div> <style>.headingStyle {
+            font-size : 64px;
+            text-align: center;
+            color: aliceblue;
+        }
+        .messageContainer {
+            padding-left: 20%;
+            padding-right: 20%;
+        }
+        .message {
+            color: aliceblue;
+        }</style>`;return{c(){e=h("div"),e.innerHTML=i},l(t){e=c(t,"DIV",{"data-svelte-h":!0}),m(e)!=="svelte-6ny45s"&&(e.innerHTML=i)},m(t,s){g(t,e,s)},p:a,i:a,o:a,d(t){t&&d(e)}}}class y extends l{constructor(e){super(),r(this,e,null,p,o,{})}}export{y as component};
