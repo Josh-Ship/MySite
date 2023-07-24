7f2a6d42c0f5452bece4c33703b619a1dcc0af33
joshua <josh.d.armour@gmail.com>
2023-07-24 17:16:02 +1000

    Init

---
+++ blog/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<h1  style="text-align:center">Welcome to Hell</h1>
+<h3> Written by ChatGPT, mostly lies</h3>
+<p>
+    Welcome to my website! This is the hub of my creative endeavors, where I showcase a variety of exciting projects
+     that I have in the works. As a passionate individual with a love for all things tech and gaming, you'll find a collection of captivating content here. 
+     Dive into the world of immersive gaming with my Unity game, explore the latest updates and insights 
+     on my blog, and engage with like-minded enthusiasts in the interactive chatroom. Whether you're a fellow 
+     developer, an avid gamer, or simply someone seeking inspiration, this blog is the perfect place to connect, 
+     learn, and share experiences. Join me on this thrilling journey as we unravel the mysteries of game development, 
+     engage in thoughtful discussions, and embark on exciting new ventures together. Let's make magic happen, one project at a time!
+</p>
+<h2>Latest Developments</h2>
+<p>
+    Making this blog site because I wanted to see if SvelteKit was any good,
+    so far, so good, I feel like it allows me to make websites very fast and 
+    easily.
+</p>
+<img src="/blogDev1.png" width="50%" height="auto">
+
