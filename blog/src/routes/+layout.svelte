a8d0a1df0bd364ec88094255b0169a9da0f08919
joshua <josh.d.armour@gmail.com>
2023-07-24 18:52:15 +1000

    added some things

---
+++ blog/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  import "../app.postcss";
+  import { NavBrand, NavHamburger, NavLi, NavUl, Navbar } from 'flowbite-svelte';
+</script>
+
+<div class="layout">
+  <Navbar let:hidden let:toggle color="dark">
+    <NavBrand>
+
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Josh's Blog
+        </span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl>
+        <NavLi href="/" active = {true}>Home</NavLi>
+        <NavLi href="/Blog">Blog</NavLi>
+        <NavLi href="/UnityGame">Unity Game</NavLi>
+    </NavUl>
+  </Navbar>
+
+  <div class="mainContent">
+    <main>
+      <slot />
+    </main>
+  </div>
+</div>
+
+
